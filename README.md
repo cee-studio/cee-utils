b4755d276d26f07da50312265ca68184b5c1d0de
mlite <mlite@users.noreply.github.com>
2021-05-19 11:10:35 -0700

    add a script to download all files

--- README.md
+++ README.md
@@ -1,7 +1,8 @@
-# json-actor
+# core-utils
 
 The code are supposed to be dowloaded to our source tree to be used. 
 
 ```
-wget 
+wget https://raw.githubusercontent.com/cee-studio/core-utils/master/download.sh -O get.sh
+sh get.sh
 ```
---
+++ get.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+url="https://raw.githubusercontent.com/cee-studio/core-utils/master"
+
+list="debug.h
+jsmn.h
+json-actor-boxed.c
+json-actor-boxed.h
+json-actor.c
+json-actor.h
+json-parser.c
+json-printf.c
+json-scanf.c
+json-scanf.h
+json-string.c
+json-struct.c
+log.c
+logconf.c
+logconf.h
+log.h
+ntl.c
+ntl.h
+orka-utils.c
+orka-utils.h
+utarray.h
+uthash.h
+utlist.h
+utringbuffer.h
+utstack.h
+utstring.h"
+
+for i in $list; do
+    echo "getting $i"
+    echo "$url/$i"
+    wget $url/$i -O $i
+done
