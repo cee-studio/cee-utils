49ab03e4ef04efcb5fd90bbd4a11a1fc38af4cd7
mlite <mlite@users.noreply.github.com>
2021-05-24 14:33:44 -0700

    add a simple hmac

--- hmac.h
+++ hmac.h
@@ -1,19 +1,4 @@
-/**
-* @file re_hmac.h  Interface to HMAC functions
-*
-* Copyright (C) 2010 Creytiv.com
-*/
-
-#ifndef HMAC_H_
-#define HMAC_H_ (1)
-
 #include <stdint.h>
+#include <stdlib.h>
 
-void hmac_sha1(const uint8_t *k,   /* secret key */
-        size_t lk,  /* length of the key in bytes */
-        const uint8_t *d,   /* data */
-        size_t ld,  /* length of data in bytes */
-        uint8_t *out, /* output buffer, at least "t" bytes */
-        size_t *t);
-
-#endif // HMAC_H_
+int hmac_sha1(uint8_t* digest, uint8_t* key, uint8_t* message, size_t keylen, size_t msglen);
