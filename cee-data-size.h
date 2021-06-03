9e1a21f8f5d508280a5e22f17507538f2c3179d0
mlite <mlite@users.noreply.github.com>
2021-06-02 19:19:00 -0700

    add data size size header

---
+++ cee-data-size.h
@@ -0,0 +1,10 @@
+#ifndef CEE_DATA_SIZE_H
+#define CEE_DATA_SIZE_H
+
+#define CEE_SHA1_DATA_LEN   40
+#define CEE_SHA1_STR_LEN    (40+1)
+
+#define CEE_MAX_IP_ADDR_STR_LEN  46   
+
+
+#endif // CEE_DATA_SIZE_H
