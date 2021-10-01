92436fb95ac2600fa88b79aba0a599baa5819e5d
mlite <mlite@users.noreply.github.com>
2021-10-01 12:14:32 -0700

    feat: add back a simple version

--- cee-sqlite3-2.h
+++ cee-sqlite3-2.h
@@ -11,17 +11,24 @@
 #include "cee-json.h"
 
 extern sqlite3* cee_sqlite3_init_db(char *dbname, char *sqlstmts);
+enum cee_sqlite3_type {
+  CEE_SQLITE3_INT,
+  CEE_SQLITE3_INT64,
+  CEE_SQLITE3_TEXT,
+  CEE_SQLITE3_BLOB
+};
+  
+struct cee_sqlite3_bind_info_data {
+  char *name; /* name for the binding variable */
+  enum cee_sqlite3_type type;
+  void *value;
+};
 
 struct cee_sqlite3_bind_info {
-  char *var_name; // name for the binding variable
-  char *col_name; // db column name
-  char *ext_name; // external name
-  enum type {
-    CEE_SQLITE3_INT,
-    CEE_SQLITE3_INT64,
-    CEE_SQLITE3_TEXT,
-    CEE_SQLITE3_BLOB
-  } type;
+  char *var_name; /* name for the binding variable */
+  char *col_name; /* db column name */
+  char *ext_name; /* external name */
+  enum cee_sqlite3_type type;
 };
 
 /*
