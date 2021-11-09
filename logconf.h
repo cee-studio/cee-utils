6956c9d5382133c36c3c3b8e7e628c864ea50004
lcsmuller <muller.lucas@Hotmail.com>
2021-11-09 15:20:33 -0300

    style(logconf): reformat with orca's .clang-format

--- logconf.h
+++ logconf.h
@@ -6,37 +6,48 @@ extern "C" {
 #endif /* __cplusplus */
 
 #include <stdint.h> /* uint64_t */
-#include "ntl.h"    /* struct sized_buffer */
+#include "ntl.h" /* struct sized_buffer */
 #include "debug.h"
 
 /** @brief Log level trace */
-#define logconf_trace(conf, ...) logconf_log(conf, LOG_TRACE, __FILE__, __LINE__, __VA_ARGS__)
+#define logconf_trace(conf, ...)                                              \
+  logconf_log(conf, LOG_TRACE, __FILE__, __LINE__, __VA_ARGS__)
 /** @brief Log level debug */
-#define logconf_debug(conf, ...) logconf_log(conf, LOG_DEBUG, __FILE__, __LINE__, __VA_ARGS__)
+#define logconf_debug(conf, ...)                                              \
+  logconf_log(conf, LOG_DEBUG, __FILE__, __LINE__, __VA_ARGS__)
 /** @brief Log level info */
-#define logconf_info(conf, ...)  logconf_log(conf, LOG_INFO,  __FILE__, __LINE__, __VA_ARGS__)
+#define logconf_info(conf, ...)                                               \
+  logconf_log(conf, LOG_INFO, __FILE__, __LINE__, __VA_ARGS__)
 /** @brief Log level warn */
-#define logconf_warn(conf, ...)  logconf_log(conf, LOG_WARN,  __FILE__, __LINE__, __VA_ARGS__)
+#define logconf_warn(conf, ...)                                               \
+  logconf_log(conf, LOG_WARN, __FILE__, __LINE__, __VA_ARGS__)
 /** @brief Log level error */
-#define logconf_error(conf, ...) logconf_log(conf, LOG_ERROR, __FILE__, __LINE__, __VA_ARGS__)
+#define logconf_error(conf, ...)                                              \
+  logconf_log(conf, LOG_ERROR, __FILE__, __LINE__, __VA_ARGS__)
 /** @brief Log level fatal */
-#define logconf_fatal(conf, ...) logconf_log(conf, LOG_FATAL, __FILE__, __LINE__, __VA_ARGS__)
+#define logconf_fatal(conf, ...)                                              \
+  logconf_log(conf, LOG_FATAL, __FILE__, __LINE__, __VA_ARGS__)
 
 /* helper function for logconf_log() */
-#define __logconf_log(conf, level, file, line, fmt, ...) _log_log(&(conf)->L, level, file, line, "[%s] "fmt"%s", (conf)->id, __VA_ARGS__)
+#define __logconf_log(conf, level, file, line, fmt, ...)                      \
+  _log_log(&(conf)->L, level, file, line, "[%s] " fmt "%s", (conf)->id,       \
+           __VA_ARGS__)
 /** @brief Run-time configurable log level */
-#define logconf_log(conf, level, file, line, ...) __logconf_log(conf, level, file, line, __VA_ARGS__, "")
+#define logconf_log(conf, level, file, line, ...)                             \
+  __logconf_log(conf, level, file, line, __VA_ARGS__, "")
 
 /** Maximum length for module id */
-#define LOGCONF_ID_LEN   64 + 1
+#define LOGCONF_ID_LEN 64 + 1
 /** Maximum length for the output file path */
 #define LOGCONF_PATH_MAX 4096
 
 /**
- * @brief A stackful and modularized wrapper over the popular 'log.c' facilities
+ * @brief A stackful and modularized wrapper over the popular 'log.c'
+ * facilities
  *
- * Provides a way to split logging into individual modules, that can be configured
- *        separately from one another, or have their resources shared via branching.
+ * Provides a way to split logging into individual modules, that can be
+ * configured separately from one another, or have their resources shared via
+ * branching.
  */
 struct logconf {
   /** logging module id */
@@ -55,7 +66,7 @@ struct logconf {
     char fname[LOGCONF_PATH_MAX];
     /** pointer to logging output file */
     FILE *f;
-  } *logger, *http;
+  } * logger, *http;
   /** list of 'id' that should be ignored */
   NTL_T(struct ja_str) disable_modules;
 };
@@ -73,25 +84,30 @@ struct loginfo {
  *
  * @param conf pointer to the `struct logconf` structure to be initialized
  * @param id the `struct logconf` module id
- * @param fp the configuration file pointer that will fill `struct logconf` fields
+ * @param fp the configuration file pointer that will fill `struct logconf`
+ * fields
  */
 void logconf_setup(struct logconf *conf, const char id[], FILE *fp);
 
 /**
  * @brief Branch and link a `struct logconf` module to a existing one
  *
- * Initialize a `branch` logging module thats expected to share common resources with 
- *        its parent parent module `orig`. The common resources include: config file
- *        directives, logging output and disabled modules list.
- * @param branch pointer to the `struct logconf` structure to be initialized as `orig` branch
- * @param orig pointer to the `struct logconf` structure that holds the parent module
+ * Initialize a `branch` logging module thats expected to share common
+ * resources with its parent parent module `orig`. The common resources
+ * include: config file directives, logging output and disabled modules list.
+ * @param branch pointer to the `struct logconf` structure to be initialized as
+ * `orig` branch
+ * @param orig pointer to the `struct logconf` structure that holds the parent
+ * module
  * @param id the `branch` module id
  */
-void logconf_branch(struct logconf *branch, struct logconf *orig, const char id[]);
+void logconf_branch(struct logconf *branch,
+                    struct logconf *orig,
+                    const char id[]);
 
 /**
  * @brief Cleanup a `struct logconf` module
- * 
+ *
  * @param conf the `struct logconf` structure to be cleaned
  */
 void logconf_cleanup(struct logconf *conf);
@@ -118,7 +134,13 @@ struct sized_buffer logconf_get_field(struct logconf *conf, char *json_field);
  *        transfer's HTTP method, or HTTP response code.
  * @param ... subsequent arguments that are converted for output
  */
-void logconf_http(struct logconf *conf, struct loginfo *info, char url[], struct sized_buffer header, struct sized_buffer body, char label_fmt[], ...);
+void logconf_http(struct logconf *conf,
+                  struct loginfo *info,
+                  char url[],
+                  struct sized_buffer header,
+                  struct sized_buffer body,
+                  char label_fmt[],
+                  ...);
 
 #ifdef __cplusplus
 }
